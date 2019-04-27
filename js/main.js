2b574a6fe5cb28777ee8d0b90352a4c2dcc71c85
ized <business@ized.online>
2019-04-27 16:22:18 +0800

    Rotation pauses when player is paused
    - Moved both major blocks of inline js into a new main.js file
    - As a result both blocks now share the same *isPlaying* variable and any other future variables.
    - Result is a cleaner html document that should be easier to populate and style without a huge chunk of js in my way

---
+++ js/main.js
@@ -0,0 +1,159 @@
+var yourAudio = document.getElementById('myAudio'),
+	ctrl = document.getElementById('button'),
+	isPlaying = false;
+
+ctrl.onclick = function () {
+	if (isPlaying) {
+		yourAudio.pause();
+		isPlaying = false;
+	} else {
+		yourAudio.play();
+		isPlaying = true;
+	}
+};
+
+//
+
+var container;
+
+var camera, scene, renderer;
+
+var mouseX = 0, mouseY = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+var object;
+
+init();
+animate();
+
+
+function init() {
+
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
+
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+	camera.position.z = 20;
+
+	// scene
+
+	scene = new THREE.Scene();
+
+	var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+	scene.add( ambientLight );
+
+	var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
+	camera.add( pointLight );
+	scene.add( camera );
+
+	// manager
+
+	function loadModel() {
+
+		object.traverse( function ( child ) {
+
+			if ( child.isMesh ) child.material.map = texture;
+
+		} );
+
+		object.position.y = 0;
+		scene.add( object );
+
+	}
+
+	var manager = new THREE.LoadingManager( loadModel );
+
+	manager.onProgress = function ( item, loaded, total ) {
+
+		console.log( item, loaded, total );
+
+	};
+
+	// texture
+
+	var textureLoader = new THREE.TextureLoader( manager );
+
+	var texture = textureLoader.load( 'textures/UV_Grid_Sm.jpg' );
+
+	// model
+
+	function onProgress( xhr ) {
+
+		if ( xhr.lengthComputable ) {
+
+			var percentComplete = xhr.loaded / xhr.total * 100;
+			console.log( 'model ' + Math.round( percentComplete, 2 ) + '% downloaded' );
+
+		}
+
+	}
+
+	function onError() {}
+
+	var loader = new THREE.OBJLoader( manager );
+
+	loader.load( 'models/obj/ized nobrain.obj', function ( obj ) {
+
+		object = obj;
+
+	}, onProgress, onError );
+
+	//
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	container.appendChild( renderer.domElement );
+
+	//document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	//document.addEventListener( 'timeadvance', getMilliseconds(), 0 );
+
+	//
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove( event ) {
+
+	mouseX = ( event.clientX - windowHalfX ) / 2;
+	mouseY = ( event.clientY - windowHalfY ) / 2;
+
+}
+
+//
+
+function animate() {
+	requestAnimationFrame( animate );
+	render();
+
+}
+
+function render() {
+
+	if (isPlaying) {
+		var x = camera.position.x;
+		var z = camera.position.z;
+	
+		camera.position.x = x * Math.cos(0.01) + z * Math.sin(0.01);
+		camera.position.z = z * Math.cos(0.01) - x * Math.sin(0.01);
+
+		camera.lookAt( scene.position );
+	};
+	renderer.render( scene, camera );
+
+}
